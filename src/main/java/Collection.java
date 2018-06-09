f200e7334bab153f6b6f594876827e9e67dc9838
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 15:27:36 +0200

    Create interface `Collection` methods

--- src/main/java/Collection.java
+++ src/main/java/Collection.java
@@ -1,3 +1,8 @@
-public interface Collection {
+import java.util.List;
 
+public interface Collection<T> {
+    boolean add(T element);
+    boolean delete(T element);
+    List<T> getAll();
+    T getShip(int index);
 }
