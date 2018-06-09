8ef12554b17b891e722b335a1ea5b7bbf4de8355
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 16:14:59 +0200

    Add toString() method to JetSki

--- src/main/java/JetSki.java
+++ src/main/java/JetSki.java
@@ -2,4 +2,12 @@ public class JetSki extends Vessel {
     public JetSki(String name, double tonnage, double length, int crew, double value, double range) {
         super(name, tonnage, length, crew, value, range);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("JetSki, ");
+        sb.append(super.toString());
+        return sb.toString();
+    }
 }
