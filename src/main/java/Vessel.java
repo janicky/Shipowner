80384a8a7e931822a5c992b6f5e61c2b3f774986
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 15:25:49 +0200

    Add fields and constructor to Vessel

--- src/main/java/Vessel.java
+++ src/main/java/Vessel.java
@@ -1,2 +1,13 @@
 public class Vessel {
+
+    private String name;
+    private double tonnage;
+    private double length;
+
+    public Vessel(String name, double tonnage, double length) {
+        this.name = name;
+        this.tonnage = tonnage;
+        this.length = length;
+    }
+
 }
