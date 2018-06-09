e6c25f83503a1fbb0d7b49b4aa9ba502415debb7
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 15:47:42 +0200

    Add some fields to Vessel

--- src/main/java/Vessel.java
+++ src/main/java/Vessel.java
@@ -3,6 +3,9 @@ public class Vessel {
     private String name;
     private double tonnage;
     private double length;
+    private int crew;
+    private double value;
+    private double range;
 
     public Vessel(String name, double tonnage, double length) {
         this.name = name;
@@ -10,4 +13,27 @@ public class Vessel {
         this.length = length;
     }
 
+    public int getCrew() {
+        return crew;
+    }
+
+    public void setCrew(int crew) {
+        this.crew = crew;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getRange() {
+        return range;
+    }
+
+    public void setRange(double range) {
+        this.range = range;
+    }
 }
