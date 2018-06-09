b4628a6f1ac2002d8133e0edd99d91176f1e0bce
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 15:30:28 +0200

    Create Shipowner class and default empty methods from interface

---
+++ src/main/java/Shipowner.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Shipowner implements Collection<Vessel> {
+
+    public boolean add(Vessel element) {
+        return false;
+    }
+
+    public boolean delete(Vessel element) {
+        return false;
+    }
+
+    public List<Vessel> getAll() {
+        return null;
+    }
+
+    public Vessel getShip(int index) {
+        return null;
+    }
+}
