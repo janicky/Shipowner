0174de3cbf3621adda9ecd63c29fb1944bfe7478
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 16:15:31 +0200

    Add toString() method to MotorBoat

--- src/main/java/Motorboat.java
+++ src/main/java/Motorboat.java
@@ -2,4 +2,12 @@ public class Motorboat extends Vessel {
     public Motorboat(String name, double tonnage, double length, int crew, double value, double range) {
         super(name, tonnage, length, crew, value, range);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("JetSki, ");
+        sb.append(super.toString());
+        return sb.toString();
+    }
 }
