db138a968ab2a9803cd3b60796dd7c3109e50624
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-09 16:15:15 +0200

    Add toString() method to Tanker

--- src/main/java/Tanker.java
+++ src/main/java/Tanker.java
@@ -2,4 +2,12 @@ public class Tanker extends Vessel {
     public Tanker(String name, double tonnage, double length, int crew, double value, double range) {
         super(name, tonnage, length, crew, value, range);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tanker, ");
+        sb.append(super.toString());
+        return sb.toString();
+    }
 }
